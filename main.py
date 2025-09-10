f619defc2227c9e07fe6c09d15c8d9e2e5413bbc
Lucas Santos <lucasforcity.dev@gmail.com>
2025-09-10 19:08:23 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-from funcoes import saudacao, soma_lista, numeros_pares
-
-print(saudacao("Lucas"))
-
-minha_lista = [1, 2, 3, 4, 5]
-print("Soma da lista:", soma_lista(minha_lista))
-
-print("Números pares até 10:", numeros_pares(10))
